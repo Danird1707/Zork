15337bde8a2118f3281bcf21b5ab2827d88f7a1a
Danird1707 <166602639+Danird1707@users.noreply.github.com>
2026-04-29 11:38:25 +0200

    Basic main with welcome and basic player input

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	string playerInput;
+	cout << "Welcome to Zork!\n";
+	cout << "Insert a number\n";
+	cin >> playerInput;
+	cout << "Your number is " << playerInput << endl;
+	return 0;
+}
