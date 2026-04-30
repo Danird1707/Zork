0a771d55bfe3e62489aaff3b982dc8946e14b254
Danird1707 <166602639+Danird1707@users.noreply.github.com>
2026-04-30 16:21:29 +0200

    Class player created

--- Player.h
+++ Player.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Creature.h"
+#include <string>
+
+class Player : public Creature {
+public:
+	Player(const std::string& m_name, const std::string& m_description, Room* location);
+	~Player();
+
+private:
+	std::string m_name;
+	std::string m_description;
+};
+#endif
